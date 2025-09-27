aac85c55264843cc67d281fdeed2141bd3195013
Sagarika <124549688+Sagarika311@users.noreply.github.com>
2025-09-27 15:56:55 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY", default="unsafe-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://django-user-authentication.onrender.com"]
 
 
 # Application definition
